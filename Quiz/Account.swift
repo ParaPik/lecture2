104ad4579072e5b80e37cb94d6a33eb4c20d7701
kamiraider <pazzl@icloud.com>
2016-07-10 14:08:46 +0300

    Initial commit

---
+++ Quiz/Account.swift
@@ -0,0 +1,13 @@
+//
+//  Account.swift
+//  Quiz
+//
+//  Created by Admin on 10.07.16.
+//  Copyright © 2016 Specialist. All rights reserved.
+//
+
+import Foundation
+
+class Account {
+    static var AccountName : String = ""
+}
---
+++ Quiz/QuizController.swift
@@ -0,0 +1,13 @@
+//
+//  QuizController.swift
+//  Quiz
+//
+//  Created by Admin on 10.07.16.
+//  Copyright © 2016 Specialist. All rights reserved.
+//
+
+import UIKit
+
+class QuizController : UIViewController {
+    
+}
